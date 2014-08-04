a70028d9c34ed2f6a1abe406ff08ee42756a1cf3
YusefOuda <yiouda92@gmail.com>
2014-08-04 18:01:14 -0500

    Add getter and setter methods

--- ruby/cookbook.rb
+++ ruby/cookbook.rb
@@ -1,10 +1,16 @@
 class Cookbook
+  attr_accessor :title
+
   def intitialize(title)
   	@title = title
   end
 end
 
 class Recipe
+  attr_accessor :title
+  attr_accessor :steps
+  attr_accessor :ingredients
+
   def initialize(title, ingredients, steps)
   	@title = title
   	@ingredients = ingredients
