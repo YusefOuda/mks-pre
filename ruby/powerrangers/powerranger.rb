38d9232ca40d40c669476b769dd6b6864d437edc
Yusef Ouda <yiouda92@gmail.com>
2014-08-06 13:38:32 -0500

    Implement punch method to PowerRanger class

--- ruby/powerrangers/powerranger.rb
+++ ruby/powerrangers/powerranger.rb
@@ -1,12 +1,14 @@
 class Person
-  def initialize(name, caffeine_level=0)
+  attr_accessor :caffeine_level
+
+  def initialize(name, caffeine_level=10)
 	  @name = name
 	  @caffeine_level = caffeine_level
   end
 
-  def run(distance)
-  	puts "#{@name} ran #{distance} miles!"
-  	@caffeine_level -= distance / 2.0
+  def run
+  	puts "#{@name} ran!"
+  	@caffeine_level -= 3
   	puts "#{@name}'s caffeine level decreased to #{@caffeine_level}."
   end
 
@@ -18,19 +20,28 @@ class Person
 
   def drink_coffee
   	puts "#{@name} drinks a large cup of super-charged coffee."
-  	@caffeine_level += 10
+  	@caffeine_level += 5
   end
 
 end
 
 class PowerRanger < Person
-  def initialize(name, caffeine_level=0, strength, color)
+  def initialize(name, caffeine_level=10, strength, color)
   	super(name, caffeine_level)
   	@strength = strength
   	@color = color
   end
 
   def punch(person)
+    if @strength < 5
+      person.scream
+      person.run
+    else
+      puts "#{person.name} was someraulted into the air!"
+      person.caffeine_level -= 5
+      puts "#{person.name}'s caffeine level was decreased to {person.caffeine_level}"
+    end
+    @caffeine_level -= 5
 end
 
 class EvilNinja
